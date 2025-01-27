d26a883921ad14bd54b436938c461c5addf78f68
apolonio <manuelapoloniocuevas@gmail.com>
2025-01-26 22:30:41 -0500

    feat: adding a model of support vector machine for predict the iris_test data set

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import altair as alt
 from sklearn.datasets import load_iris
 import pandas as pd
 import plotly.express as px
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+
 
 st.subheader('Apolonio Manuel')
 
@@ -53,4 +56,35 @@ chart_3d = px.scatter_3d(iris_df,
                          )
 
 st.plotly_chart(chart_3d)
+
+st.title('Train a simple model')
+
+X = iris_df.drop(['target', 'species'], axis=1)
+y = iris_df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+model = SVC()
+
+model.fit(X_train, y_train)
+
+# el usuario puede seleccionar un registro para que el modelo haga una predicción
+
+st.title('Make a prediction')
+
+# seleccionar un registro de test de forma interactiva
+
+index = st.selectbox('Select a test sample', range(len(X_test)))
+
+if index is not None:
+    # mostrar los datos del registro seleccionado
+    selected_data = X_test.iloc[index]
+    st.write(selected_data)
+    st.write(f'The target is: {iris.target_names[y_test.iloc[index]]}')
+
+    # hacer una predicción
+    prediction = model.predict([selected_data])
+    st.write(f'The prediction is: {iris.target_names[prediction][0]}')
+
 
